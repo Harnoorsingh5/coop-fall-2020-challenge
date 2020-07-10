e76fdd1f7f9cd311efccc64fbeeae0084d6e1188
Harnoor Singh <harnoor.singh539@gmail.com>
2020-07-10 14:08:43 -0400

    all test cases passes - Python - Fall20

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,49 @@
 class EventSourcer():
     # Do not change the signature of any functions
-
+    
     def __init__(self):
         self.value = 0
+        self.operations = []
+        self.operands = []
+        self.undo_operations = 0
 
     def add(self, num: int):
-        pass
+        self.value += num
+        self.operations.append('a')
+        self.operands.append(num)
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self.operations.append('s')
+        self.operands.append(num)
 
     def undo(self):
-        pass
+        for i in range(len(self.operations)-self.undo_operations-1,-1,-1):
+            if self.operations[i] == 'a':
+                self.value -= self.operands[i]
+            else:
+                self.value += self.operands[i]
+            self.undo_operations += 1
+            break
 
     def redo(self):
-        pass
+        for i in range(len(self.operations)-self.undo_operations-1,-1,-1):
+            if self.operations[i] == 'a':
+                self.value += self.operands[i]
+            else:
+                self.value -= self.operands[i]
+            break
 
     def bulk_undo(self, steps: int):
-        pass
+        for i in range(len(self.operations)-1,len(self.operations)-1-steps,-1):
+            if self.operations[i] == 'a':
+                self.value -= self.operands[i]
+            else:
+                self.value += self.operands[i]
 
     def bulk_redo(self, steps: int):
-        pass
+        for i in range(len(self.operations)-1,len(self.operations)-1-self.undo_operations,-1):
+            if self.operations[i] == 'a':
+                self.value += self.operands[i]
+            else:
+                self.value -= self.operands[i]
